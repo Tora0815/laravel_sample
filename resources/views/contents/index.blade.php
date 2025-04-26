17556324862ef042ee29c6dafb7ace0ecb2cd5f8
Tora0815 <codetora6@gmail.com>
2025-04-26 23:12:34 +0900

    feat: index.blade.phpを作成（書籍181p：みんなのアルバム一覧ビュー）

--- resources/views/contents/index.blade.php
+++ resources/views/contents/index.blade.php
@@ -5,15 +5,41 @@
 --}}
 
 <x-page-base>
-    <x-slot name="title">ホーム</x-slot>
+    {{-- ページタイトル --}}
+    <x-slot name="title">みんなのアルバム</x-slot>
 
+    {{-- ページメインコンテンツ --}}
     <x-slot name="slot">
-        <div class="container mx-auto px-4">
-            <h1 class="text-xl font-semibold text-gray-800">サンプルページ</h1>
+        <div class="container">
+            <div class="row mt-5">
+                <div class="col-12">
+                    <h1 class="text-center">みんなのアルバム</h1>
+                </div>
+            </div>
+
+            <div class="row mt-5">
+                @foreach ($image_array as $image)
+                    <div class="col-sm-3 p-2">
+                        <div class="card p-1">
+                            <form method="post" action="/show_album">
+                                @csrf
+                                <input type="hidden" name="u_id" value="{{ $image['id'] }}">
+                                <p class="text-center">{{ $image['user_name'] }}</p>
+                                <p class="text-center">
+                                    <input type="image" src="{{ $image['img'] }}" class="w-100 album pointer" value="{{ $image['id'] }}">
+                                </p>
+                            </form>
+                        </div>
+                    </div>
+                @endforeach
+            </div>
         </div>
     </x-slot>
 
+    {{-- JavaScript用slot --}}
     <x-slot name="script">
-        {{-- 必要ならJSをここに --}}
+        <script>
+            // ここに必要ならJS処理を追記
+        </script>
     </x-slot>
 </x-page-base>
