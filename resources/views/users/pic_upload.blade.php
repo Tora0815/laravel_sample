eeb64829af5a11ab1a8bb992f8e795ffb202f847
Tora0815 <codetora6@gmail.com>
2025-04-29 02:45:20 +0900

    wip: pic_uploadページの一時保存（jQuery UI未対応・loading.gifなし）

--- resources/views/users/pic_upload.blade.php
+++ resources/views/users/pic_upload.blade.php
@@ -15,13 +15,14 @@
         </div>
     </x-slot>
 
+    {{-- メインコンテンツ --}}
     <x-slot name="slot">
         @csrf
 
-        {{-- ユーザーID表示 (静的にhidden) --}}
-        <div id="u_id" class="hidden">{{ Auth::user()->name }}</div>
+        {{-- ユーザーID（非表示） --}}
+        <div id="u_id" class="hidden">{{ Auth::user()->id }}</div>
 
-        {{-- 写真アップロードフォーム --}}
+        {{-- ファイルアップロード --}}
         <div class="container">
             <div class="row">
                 <div class="col-12">
@@ -32,9 +33,8 @@
             <div class="row mt-5 pb-3">
                 <div class="col-md-4">&nbsp;</div>
 
-                {{-- ファイルアップロードボタン --}}
                 <div class="col-md-4 d-grid">
-                    <button type="button" id="file_up_bt" class="btn btn-primary"> ファイルアップロード </button>
+                    <button type="button" id="file_up_bt" class="btn btn-primary">ファイルアップロード</button>
                     <input type="file" id="select_file" style="display: none" multiple>
                     <input type="file" id="change_file" style="display: none">
                 </div>
@@ -45,18 +45,18 @@
 
         <hr>
 
-        {{-- 写真一覧表示 --}}
+        {{-- ファイルリスト --}}
         <div class="row mt-3">
             <div class="col-12" id="message"></div>
             <div id="list_area"></div>
         </div>
 
-        {{-- モーダル表示部 --}}
+        {{-- モーダル部 --}}
         <button type="button" id="modal_bt" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal" style="display: none">
             Launch demo modal
         </button>
 
-        {{-- 写真選択用モーダル --}}
+        {{-- ファイル選択モーダル --}}
         <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
             <div class="modal-dialog modal-xl">
                 <div class="modal-content">
@@ -75,15 +75,15 @@
 
         {{-- 削除確認ダイアログ --}}
         <div id="delete_dialog" title="削除の確認">
-            <p>ファイルを削除してよいですか？</p>
+            <p>ファイルを削除してもよろしいですか？</p>
         </div>
 
-        {{-- アップロード中表示 --}}
+        {{-- アップロード中ダイアログ --}}
         <div id="upload_dialog" title="ファイルアップロード中" class="d-flex justify-content-center p-3">
             <img src="{{ asset('images/loading.gif') }}" class="w-25">
         </div>
 
-        {{-- タイトル変更用ダイアログ --}}
+        {{-- タイトル変更ダイアログ --}}
         <div id="dialog-form" title="タイトル変更">
             <div class="mb-3">
                 <label for="set_title" class="form-label">タイトルを入力</label>
@@ -91,8 +91,87 @@
             </div>
         </div>
 
-        {{-- ここからJavaScript記述 --}}
-        @vite(['resources/js/app.js'])
+        {{-- JavaScriptエリア --}}
+        {{-- jQueryの読み込み（必ず先に読み込む） --}}
+        <script src="https://code.jquery.com/jquery-3.7.1.min.js"></script>
 
+        {{-- ここから独自JS --}}
+        <script>
+            var page_num = 0;
+
+            function changeContents(num) {
+                page_num = num;
+                document.body.style.cursor = 'wait';
+                let code = document.querySelector('[name="_token"]').value;
+                let user_id = document.getElementById('u_id').textContent.trim();
+                var fd = new FormData();
+                fd.append("_token", code);
+                fd.append("u_id", user_id);
+                fd.append("page_num", num);
+
+                $.ajax({
+                    url: '/user_pics',
+                    type: 'POST',
+                    data: fd,
+                    processData: false,
+                    contentType: false,
+                    timeout: 10000,
+                    error: function(xhr, status, error) {
+                        alert(error);
+                        document.body.style.cursor = 'auto';
+                    },
+                    beforeSend: function() {},
+                    success: function(res) {
+                        document.body.style.cursor = 'auto';
+                        $('#list_area').html(res);
+                    }
+                });
+            }
+
+            $(function() {
+                $('#file_up_bt').on('click', function() {
+                    $('#select_file').trigger('click');
+                });
+
+                $('#select_file').on('change', function(e) {
+                    $('#upload_dialog').dialog('open');
+                    for (var i = 0; i < e.target.files.length; i++) {
+                        uploadData(e.target.files[i], e.target.files[i].name);
+                    }
+                    $('#upload_dialog').dialog('close');
+                });
+            });
+
+            function uploadData(file, name) {
+                let code = document.querySelector('[name="_token"]').value;
+                let user_id = document.getElementById('u_id').textContent.trim();
+                var fd = new FormData();
+                fd.append("_token", code);
+                fd.append("upfile", file);
+                fd.append("u_id", user_id);
+
+                $.ajax({
+                    url: '/save_pics',
+                    type: 'POST',
+                    data: fd,
+                    processData: false,
+                    contentType: false,
+                    timeout: 10000,
+                    error: function(xhr, status, error) {
+                        alert(error);
+                        document.body.style.cursor = 'auto';
+                    },
+                    beforeSend: function() {},
+                    success: function(res) {
+                        document.body.style.cursor = 'auto';
+                        $('#message').html(res);
+                        changeContents(0);
+                    }
+                });
+            }
+        </script>
+
+        {{-- Viteビルド後のJS読み込み --}}
+        @vite(['resources/js/app.js'])
     </x-slot>
 </x-app-layout>
